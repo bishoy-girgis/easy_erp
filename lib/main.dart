d76bba2f61dd42eb6bc3c33718b6b76e364a0796
bishoy-girgis <girgisbishoy6@gmail.com>
2024-04-27 14:53:42 +0200

    refactor all comments and prints

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'data/models/item_model/item_model.dart';
