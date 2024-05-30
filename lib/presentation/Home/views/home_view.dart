9ea8a62c2358db0e86f9931b64b65d7ca41b5c63
bishoy-girgis <girgisbishoy6@gmail.com>
2024-05-30 15:42:09 +0300

    adjust upgrader

--- lib/presentation/Home/views/home_view.dart
+++ lib/presentation/Home/views/home_view.dart
@@ -1,35 +1,52 @@
 import 'package:easy_erp/core/helper/app_constants.dart';
 import 'package:easy_erp/core/widgets/gap.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:upgrader/upgrader.dart';
 import '../../Login/views/widgets/change_language_section.dart';
 import '../view_models/categories/categories.dart';
-import 'widgets/category_widget.dart';
 import 'widgets/home_view_header_card.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    var l = AppLocalizations.of(context)!;
+  State<HomeView> createState() => _HomeViewState();
+}
 
-    return
-      // UpgradeAlert(
-      // onUpdate: () => true,
-      // upgrader: Upgrader(
-      //   debugLogging: true,
-      //   debugDisplayAlways: true,
-      //   durationUntilAlertAgain: const Duration(days: 1),
-      // ),
-      // dialogStyle: UpgradeDialogStyle.cupertino,
-      // showIgnore: false,
-      // showLater: false,
-      // showReleaseNotes: false,
-      // child:
-      Scaffold(
+class _HomeViewState extends State<HomeView> {
+  String? version;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeVersion();
+  }
+
+  Future<void> _initializeVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      version = packageInfo.version;
+      debugPrint("VERSION Is  $version");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return UpgradeAlert(
+      onUpdate: () => true,
+      upgrader: Upgrader(
+        debugLogging: true,
+        debugDisplayAlways: true,
+        durationUntilAlertAgain: const Duration(days: 1),
+        minAppVersion: version,
+      ),
+      dialogStyle: UpgradeDialogStyle.material,
+      showIgnore: false,
+      showLater: false,
+      showReleaseNotes: false,
+      child: Scaffold(
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
@@ -63,7 +80,7 @@ class HomeView extends StatelessWidget {
             ),
           ),
         ),
-      // ),
+      ),
     );
   }
 }
